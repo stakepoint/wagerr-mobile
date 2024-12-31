5d07d6f3039fd8ab4830e194502a990368da3e4d
hoossayn <abdulhakeemhusseni@gmail.com>
2024-12-31 15:16:04 +0100

    chore: update gradle and fix errors

--- lib/main.dart
+++ lib/main.dart
@@ -100,7 +100,6 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
