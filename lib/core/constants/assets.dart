c80c7b1dad54816aa76355d194d747945449dd0e
PeterOche <petergoddey08@gmail.com>
2025-02-04 11:43:06 +0100

    merge dev into feature/STRKWGR-008

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -21,6 +21,41 @@ class AppIcons {
   static const infoIcon = 'assets/icons/infoIcon.svg';
   static const hashTagIcon = 'assets/icons/hashTagIcon.svg';
   static const fundWalletIcon = 'assets/icons/wallet_icon.svg';
+
+  //-------------------------------------------- IMAGES -------------------------------------------- //
+  static const userImage = 'assets/images/user.png';
+  static const starknetImage = 'assets/images/starknet.png';
+  static const String awaitingUserPath = 'assets/images/awaitingUser.png';
+
+  static const String baseIconPath = 'assets/icons';
+  static const String userPath = '$baseIconPath/user.png';
+  static const String copyPath = '$baseIconPath/copy.svg';
+  static const String notificationPath = '$baseIconPath/notification.svg';
+  static const String snSymbol = '$baseIconPath/sn_symbol.png';
+  static const String arrowBack = '$baseIconPath/arrow_back.svg';
+}
+class AppIcons {
+  AppIcons._();
+
+  static const hamburgerIcon = 'assets/icons/hamburger_icon.svg';
+  static const argentIcon = 'assets/icons/argent_icon.png';
+  static const braavosIcon = 'assets/icons/braavos_icon.png';
+  static const metaMaskIcon = 'assets/icons/metamask_icon.svg';
+  static const profileDummy = 'assets/icons/profile_dummy.png';
+  static const copyIcon = 'assets/icons/copy.svg';
+  static const notificationIcon = 'assets/icons/notification.svg';
+  static const addIcon = 'assets/icons/add.svg';
+  static const withdrawIcon = 'assets/icons/withdraw.svg';
+  static const noWaggerIcon = 'assets/icons/noWagger.svg';
+  static const handshakeIcon = 'assets/icons/handshake.svg';
+  static const homeShakeIcon = 'assets/icons/homeShake.svg';
+  static const homeNoneIcon = 'assets/icons/homeNone.svg';
+  static const homeGreenIcon = 'assets/icons/homeGreen.svg';
+  static const profileIcon = 'assets/icons/profile.svg';
+  static const walletIcon = 'assets/icons/wallet.svg';
+  static const logoIcon = 'assets/icons/logo.svg';
+  static const infoIcon = 'assets/icons/infoIcon.svg';
+  static const hashTagIcon = 'assets/icons/hashTagIcon.svg';
   static const ellipseIcon = 'assets/icons/ellipse.svg';
   static const vsIcon = 'assets/icons/VS.svg';
 
