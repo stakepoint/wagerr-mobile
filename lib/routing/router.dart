274e903acfa7310cd5f6d1ab48b7a4bb9064f8b2
OnyemaAnthony <Anthonychibuike246@gmail.com>
2025-02-10 13:11:43 +0100

    fix walletscreen import

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:starkwager/core/constants/screen_layout.dart';
+import 'package:starkwager/features/wallet_screen.dart/wallet_screen.dart';
 
 import '../features/feature.dart';
 import '../routing/routes.dart';
