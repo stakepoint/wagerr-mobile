5eb0d4162185452a49ce1323be49b378dbf78a9a
Gabito1451 <obiagaziegabriel@gmail.com>
2025-02-15 23:24:23 +0100

    fix

--- lib/features/wallet_screen/widget/wallet_screen_body.dart
+++ lib/features/wallet_screen/widget/wallet_screen_body.dart
@@ -48,9 +48,7 @@ class WalletScreenBody extends ConsumerWidget {
         ),
         const SizedBox(height: 16),
         if (isMobile) HomeAddAndWithdraw(),
-        isMobile
-            ? const SizedBox(height: 48)
-            : const SizedBox(height: 40),
+        isMobile ? const SizedBox(height: 48) : const SizedBox(height: 40),
         RecentTransactions(isTablet: !isMobile),
       ],
     );
